379a389c877b9f9cf65ddfbb6b3a0a6e3fbd931a
swift260919 <swift260919@gmail.com>
2019-10-28 16:26:31 +0200

    lec4final

--- Lec4Part3/Lec4Part3/ViewController.swift
+++ Lec4Part3/Lec4Part3/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var cards: [UIButton]!
+    var images = [
+        UIImage(named: "icons8-cat"),
+        UIImage(named: "icons8-dog"),
+        UIImage(named: "icons8-year_of_monkey"),
+        UIImage(named: "icons8-lion"),
+        UIImage(named: "icons8-puffin_bird"),
+        UIImage(named: "icons8-cow")
+    ]
+    
+    
+    var gameImages: [UIImage?] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startGame()
     }
-
-
+    
+    func startGame(){
+        //task - choose 6 images from the images array and fill the game Images array
+        images.shuffle()
+        //todo : take only 6 first images
+        gameImages = images + images
+        gameImages.shuffle()
+        print()
+        
+        for i in 0..<cards.count{
+            let btn = cards[i]
+            
+            btn.setImage(gameImages[i], for: UIControl.State.highlighted)
+        }
+    }
+    
+    
 }
 
