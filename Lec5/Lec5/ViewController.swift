b8f12564f36caf772bd146fb0cb54841dfa3c4f5
swift260919 <swift260919@gmail.com>
2019-10-31 10:29:35 +0200

    tada

--- Lec5/Lec5/ViewController.swift
+++ Lec5/Lec5/ViewController.swift
@@ -14,12 +14,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        initGame()
+    }
+    
+    func initGame(){
+        //set the back image on all the cards:
+        let b = UIImage(named: "back")
+        for btn in memoryCards{
+            btn.setImage(b, for: .normal)
+        }
+        
+        var paths = Bundle.main.paths(forResourcesOfType: ".png", inDirectory: nil)
+        
+        //an empty image array
+        var imagesForGame = [UIImage?]()
+        
+        //3 random images for our game:
+        for _ in 0..<3{
+            //0 to paths.count
+            let rand = Int.random(in: 0..<paths.count)
+            let randPath = paths[rand]
+            
+            //optional image -> maybe there is no image under the given path
+            let image = UIImage(contentsOfFile: randPath)
+            imagesForGame.append(image)
+        }
+        
+        imagesForGame = imagesForGame + imagesForGame // [] = [] + []
+        imagesForGame.shuffle()
+        
+        
+        //fill the game cards in the buttons
+//        for i in 0..<imagesForGame.count{
+//            memoryCards[i].setImage(imagesForGame[i], for: .highlighted)
+//        }
+//
+        //same as the solution above.
+        for(image, btn) in zip(imagesForGame, memoryCards){
+            btn.setImage(image, for: .highlighted)
+        }
+        //given: imagesForGame
     }
 
     
     @IBAction func cardTapped(_ sender: UIButton) {
+        print("Tapped")
+        //get the highlighted image
+        let img = sender.image(for: .highlighted)
+        //set the image for the normal state
+        sender.setImage(img, for: .normal)
     }
-    
-
+ 
 }
 
