245bc4a003653a6ed04282ee0c7d6ac523702955
swift260919 <swift260919@gmail.com>
2019-10-31 09:30:27 +0200

    h

--- Lec5/Lec5/ViewController.swift
+++ Lec5/Lec5/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var memoryCards: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func cardTapped(_ sender: UIButton) {
+    }
+    
 
 }
 
