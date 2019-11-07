16f090d105c1a92503fea8adc8c1db81fc79bf03
swift260919 <swift260919@gmail.com>
2019-11-07 10:28:23 +0200

    up

---
+++ Lec7/Lec7/VegCellTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  VegCellTableViewCell.swift
+//  Lec7
+//
+//  Created by hackeru on 07/11/2019.
+//  Copyright © 2019 hackeru. All rights reserved.
+//
+
+import UIKit
+
+
+//cache the findViewByTag
+class VegCellTableViewCell: UITableViewCell {
+ //ref to your views
+    @IBOutlet weak var vegImageView: UIImageView!
+    
+    @IBOutlet weak var vegLabel: UILabel!
+}
--- Lec7/Lec7/ViewController.swift
+++ Lec7/Lec7/ViewController.swift
@@ -9,11 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Todo: get this from the internet (json)
+    let arr = VegDataSource().vegtables //[Vegtable]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+}
+
+
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        //get a vegtable from the array:
+        let veg = arr[indexPath.row]
+        
+        //cell identifier from IB
+        let cell = tableView.dequeueReusableCell(withIdentifier: "vegcell", for: indexPath)
+        
+        //set the imageview.image = veg.image
+        cell.imageView?.image = veg.image
+        
+        //title label:
+        cell.textLabel?.text = veg.name
+        
+        //detail label:
+        cell.detailTextLabel?.text = veg.color
+        
+        return cell
+    }
+
 }
 
 
