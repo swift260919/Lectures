c3e520b35f371c78642cfa839d8dfcab6074d2f2
swift260919 <swift260919@gmail.com>
2019-11-18 16:25:07 +0200

    userDefaults

---
+++ Lec10Part2/Lec10Part2/UserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserViewController.swift
+//  Lec10Part2
+//
+//  Created by hackeru on 18/11/2019.
+//  Copyright © 2019 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
