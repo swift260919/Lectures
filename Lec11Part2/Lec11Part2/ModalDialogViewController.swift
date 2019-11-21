2fc578741b5c15e7a6a30d585e9272586b13ea4a
swift260919 <swift260919@gmail.com>
2019-11-21 16:10:01 +0200

    Lec11

---
+++ Lec11Part2/Lec11Part2/ModalDialogViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ModalDialogViewController.swift
+//  Lec11Part2
+//
+//  Created by hackeru on 21/11/2019.
+//  Copyright © 2019 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class ModalDialogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //TODO: animate in view did appear
+        //factory method - to present
+        //showModal(vc, completion)
+        //delegate
+        
+        //swipe to dismiss
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(swiped(_:)))
+        swipe.direction = .down
+        
+        view.addGestureRecognizer(swipe)
+    }
+    
+    @objc func swiped(_ sender: UISwipeGestureRecognizer){
+        //print("Swiped down")
+        dismiss(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lec11Part2/Lec11Part2/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Lec11Part2
+//
+//  Created by hackeru on 21/11/2019.
+//  Copyright © 2019 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBAction func showModal(_ sender: UIButton) {
+        let sb = UIStoryboard(name: "ModalDialog", bundle: nil)
+        guard let modalVC = sb.instantiateInitialViewController() else {return}
+        
+        modalVC.modalPresentationStyle = .overCurrentContext
+        
+        present(modalVC, animated: true)
+        
+    }
+    
+    
+    
+    @IBOutlet weak var height: NSLayoutConstraint!
+    
+    @IBAction func dismissDialog(_ sender: UIButton) {
+         height.constant = 0
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 15, options: [], animations: {
+                   
+                   self.view.layoutIfNeeded()
+                   
+            }, completion: nil)
+    }
+    
+    
+    @IBAction func showDialog(_ sender: UIButton) {
+        
+        sender.layer.cornerRadius = 10
+        sender.backgroundColor = UIColor.black
+        sender.tintColor = .white
+        height.constant = height.constant == 200 ? 0 : 200
+        
+//        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut], animations: {
+//            //
+//        })
+        
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 15, options: [], animations: {
+            //we want the changes to be animated
+            self.view.layoutIfNeeded()
+            
+        }, completion: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
