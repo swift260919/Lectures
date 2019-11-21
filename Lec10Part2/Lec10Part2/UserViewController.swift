0ade1775f65c5008b0359afc7a41146913605d09
swift260919 <swift260919@gmail.com>
2019-11-21 09:09:39 +0200

    lec10

--- Lec10Part2/Lec10Part2/UserViewController.swift
+++ Lec10Part2/Lec10Part2/UserViewController.swift
@@ -10,21 +10,80 @@ import UIKit
 
 class UserViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var moodSegemnted: UISegmentedControl!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.delegate = self //extension: conform to protocols
+        present(imagePickerVC, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func homeTapped(_ sender: UIButton) {
+        let name = firstNameTextField.text ?? ""
+        let last = lastNameTextField.text ?? ""
+        let index = moodSegemnted.selectedSegmentIndex //0
+        
+        if let imageData = imageView.image?.pngData(){
+            UserDefaults.standard.set(imageData, forKey: "image")
+        }
+        
+        //let moodTitle = moodSegemnted.titleForSegment(at: index) ?? ""
+        UserDefaults.standard.set(name, forKey: "name")
+        UserDefaults.standard.set(last, forKey: "last")
+        UserDefaults.standard.set(index, forKey: "index")
+        
+        
+        
+        dismiss(animated: true)
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadFromDefaults()
+    }
+    
+    func loadFromDefaults(){
+        let defaults = UserDefaults.standard
+        
+        guard let name = defaults.string(forKey: "name"),
+              let last = defaults.string(forKey: "last")
+            else{return}
+        
+        let index = defaults.integer(forKey: "index")
+        
+        firstNameTextField.text = name
+        lastNameTextField.text = last
+        moodSegemnted.selectedSegmentIndex = index
+        
+        if let imageData = defaults.data(forKey: "image"){
+            let image = UIImage(data: imageData)
+            imageView.image = image
+        }
+        
+        //let btn = UIButton()
+        //btn.layer.cornerRadius = 10
+    }
+}
 
+
+extension UserViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //cancled
+        print("Cancled")
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //[] print("Info", info)
+ 
+        picker.dismiss(animated: true)
+        
+        
+        guard let image = info[.originalImage] as? UIImage else {return}
+        imageView.image = image
+    }
 }
--- Lec10Part2/Lec10Part2/ViewController.swift
+++ Lec10Part2/Lec10Part2/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //present a modal from code:
+    @IBAction func user(_ sender: UIBarButtonItem) {
+        //present user VC modally:
+        
+        //0) get a ref to the story board
+        //we have storyboard
+        //1) init the VC (story board + id)
+        guard let vc = storyboard?.instantiateViewController(identifier: "user")  else {return}
+        //may pass data:
+        //vc.data = ""
+        //2) present(vc)
+        present(vc, animated: true) //to undo - dismiss from targer
+        
+        //same as what show segue does for you
+        //self.navigationController?.pushViewController(vc, animated: true) //to undo - navigationController?.popViewController(true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
